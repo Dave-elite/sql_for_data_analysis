69390eedeaca6c23d36e8b602a5f48233d792535
Dave-elite <davenjenga098@gmail.com>
2025-03-07 20:19:58 +0300

    SQL for data analysis problem 2 solved

--- sql_problems.sql
+++ sql_problems.sql
@@ -9,3 +9,16 @@ SELECT
 FROM job_postings_fact
 WHERE salary_year_avg > 65000
 
+-- PROBELM 2 SQL FOR DATA ANALYSIS --
+-- From the job_posting_fact return the follwoing columns: job_id, job_title_short,job_location, job_via, 
+-- job_posted_date, and salary_year_avg. Also, rename the following: job_via to job_posted_site and salary_year_avg to avg_yearly_salary 
+
+SELECT 
+	job_id, 
+    job_title_short, 
+    job_location, 
+    job_via AS job_posted_site,
+    salary_year_avg AS avg_yearly_salary
+FROM job_postings_fact
+
+
