a6fd44f76a3ef24c4ec07626a8d21aa3b3b35430
Dave-elite <davenjenga098@gmail.com>
2025-03-08 21:09:17 +0300

    Null dealing with null values SQL For data analyst

--- Sql_basics.sql
+++ Sql_basics.sql
@@ -197,7 +197,22 @@ GROUP BY
 	project_id
 
     
+ --- Working with null values
  
+SELECT 
+	job_title_short,
+    job_location,
+    job_via, 
+    salary_year_avg
+	
+	
+FROM job_postings_fact
+WHERE
+	salary_year_avg IS NOT NULL
+ORDER BY 
+	salary_year_avg
+
+
 
 
  
